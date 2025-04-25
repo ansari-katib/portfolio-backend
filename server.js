8117e174e52c4e9fe7a096d6c73081405adce633
ansari katib <ansarikatib647@gmail.com>
2025-04-25 11:48:59 +0530

    c

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(body.json())
